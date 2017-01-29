53fd1b7758dc2d1e1d708929bd89c8cb46fcb219
Tony Vähävuori <h9234@student.jamk.fi>
2017-01-29 18:07:46 +0200

    Tehty lisöö.

---
+++ teht02/Pesukone.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht01
+{
+    class Pesukone
+    {
+        // properties
+        public string Malli;
+        public int PvesiLtila;
+        public bool Linkous;
+        public bool Siliava;
+        public bool Huuhteluaine;
+        public bool Paallaolo;
+
+
+        public Pesukone(string malli)
+        {
+            Malli = malli;
+        }
+        // method to washing water heat
+        public void LisaaLtila()
+        {
+            PvesiLtila += 20;
+        }
+        public void VahLtila()
+        {
+            PvesiLtila -= 20;
+        }
+
+
+        public void OnLinkous()
+        {
+            Linkous = true;
+        }
+        public void EiLinkous()
+        {
+            Linkous = false;
+        }
+        public void OnSiliava()
+        {
+            Siliava = true;
+        }
+        public void EiSiliava()
+        {
+            Siliava = false;
+        }
+        public void OnHuuhtelu()
+        {
+            Huuhteluaine = true;
+        }
+        public void EiHuuhtelu()
+        {
+            Huuhteluaine = false;
+        }
+        public void Sammuta()
+        {
+            Paallaolo = false;
+        }
+        public void Kaynnista()
+        {
+            Paallaolo = true;
+        }
+        // Huuhteluaine is false on default
+        public Pesukone(bool Huuhteluaine)
+        {
+            Huuhteluaine = false;
+        }
+        // Linkous is true on default
+        public Pesukone()
+        {
+            Linkous = true;
+        }
+
+        // method to display data
+        public void PrintData()
+        {
+            Console.WriteLine("Mikä pesukone: " + Malli);
+            Console.WriteLine("Pesukone päällä: " + Paallaolo);
+            Console.WriteLine("Veden lämpötila: " + PvesiLtila);
+            Console.WriteLine("Linkous: " + Linkous);
+            Console.WriteLine("Siliävä pesu: " + Siliava);
+            Console.WriteLine("Huuhteluaine: " + Huuhteluaine);
+        }
+    }
+}
---
+++ teht02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pistetään pesukone pyärimään
+            Pesukone pesukone = new Pesukone();
+            pesukone.Malli = "Whirlpool";
+            pesukone.Paallaolo = true;
+            pesukone.PvesiLtila = 60;
+            pesukone.Siliava = true;
+            
+            // Printataan pesukoneen data
+            pesukone.PrintData();
+            Console.ReadLine();
+
+            // Kikka kolmonen jnejnejne.
+            Pesukone pesukone2 = new Pesukone();
+            pesukone2.Malli = "Superpesuri";
+            pesukone2.Paallaolo = true;
+            pesukone2.PvesiLtila = 40;
+            pesukone2.LisaaLtila();
+            pesukone2.LisaaLtila();
+            pesukone2.Siliava = false;
+            pesukone2.EiLinkous();
+            pesukone2.OnHuuhtelu();
+
+            // Uusi printtaus
+            pesukone2.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
